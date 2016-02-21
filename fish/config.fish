eab410bb8deaec9df6cb74638b2372c02f2e762e
jupter24 <jupiter24@users.noreply.github.com>
2016-02-21 09:03:51 +0100

    Add config for termite and i3

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,7 @@
+# for better colors in nvim
 set -x NVIM_TUI_ENABLE_TRUE_COLOR 1
+# set default terminal to termite; used by i3-sensible-terminal
+set -x TERMINAL termite
+set -x EDITOR nvim
+# don't show greeting
+set fish_greeting=""
